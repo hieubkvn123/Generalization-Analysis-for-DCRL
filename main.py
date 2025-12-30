e5503b3ac1535ea8538db7e7aba0ca9b12b85a92
hieubkvn123 <hieubkvn123@gmail.com>
2025-12-30 15:07:54 +0700

    Checkpoint

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ TRAIN_LOSS_THRESHOLD = 0.05 # 1e-2
 
 # Constants for ablation study
 MIN_WIDTH = 1
-MAX_WIDTH = 2 # 8
+MAX_WIDTH = 8
 MIN_DEPTH = 2
-MAX_DEPTH = 3 # 10
+MAX_DEPTH = 10
 DATASET_TO_INDIM = {
     'mnist': 28 * 28,          # 784 for flattened, or use (1, 28, 28) for CNNs
     'fashionmnist': 28 * 28,   # 784 for flattened, or use (1, 28, 28) for CNNs
